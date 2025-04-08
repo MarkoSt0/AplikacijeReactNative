996d73aadf036bc2e24b4411a5774c25aa6450c8
MarkoSt0 <markostevic2003@gmail.com>
2025-04-08 16:33:02 +0200

    Final day 1

--- src/app/aplikacije/aplikacija1/index.tsx
+++ src/app/aplikacije/aplikacija1/index.tsx
@@ -5,10 +5,18 @@ import { Stack } from "expo-router";
 const DetaljiAplikacije = () => {
   return (
     <View>
-      <Stack.Screen options={{ title: "Druga strana" }} />
-      <Text style={{ fontFamily: "Inter", fontSize: 50 }}>
-        DetaljiAplikacije1
+      <Stack.Screen options={{ title: "Aplikacija1" }} />
+      <Text style={{ fontFamily: "Inter", fontSize: 15 }}>
+        U sustini cela aplikacija.
       </Text>
+      <Text>Nauceno: Pravljenje projekta, organizacija, fajlovi</Text>
+      <Text>FlatList, SplashScreen(Kod ucitavanja), Ucitavanje fontova</Text>
+      <Text>Rad sa _layout.tsx(Nesto sto ce svaki korisnicki UI imati)</Text>
+      <Text>Ubacivanje u _layout.tsx Stack ili Slot</Text>
+      <Text>ExpoRouter (kako da iammo vise prozora)</Text>
+      <Text>Kreiranje komponenti</Text>
+      <Text>Expo TS, aliasi! (kako da komponente lakse referenciramo)</Text>
+      <Text></Text>
     </View>
   );
 };
